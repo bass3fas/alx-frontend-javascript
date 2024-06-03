a78e3e2581a262c5b019348f7a57e89b2253d38b
Bassant Aboelhassan <115794820+bass3fas@users.noreply.github.com>
2024-06-03 08:47:19 +0400

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,5 +1,11 @@
 export default class HolbertonClass {
   constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    } else if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
     this._size = size;
     this._location = location;
   }
@@ -7,7 +13,7 @@ export default class HolbertonClass {
   [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this._size;
-    } else if (hint === 'string') {
-      return this._location;
     }
+    return this._location;
   }
+}

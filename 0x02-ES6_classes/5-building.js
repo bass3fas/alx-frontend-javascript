0c95083045636342489317f3a54c170ff5a88997
Bassant Aboelhassan <115794820+bass3fas@users.noreply.github.com>
2024-06-03 08:46:09 +0400

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,14 @@
 export default class Building {
-  constructor(sqft) {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
     this._sqft = sqft;
-    console.log(this.#evacuationWarningMessage())
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
- #evacuationWarningMessage(){
-  return 'Hey There'
- }
+}

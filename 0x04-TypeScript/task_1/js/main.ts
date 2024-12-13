f219c41e080b10f68c34e6f404132afd8cf842f7
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-13 11:18:28 +0400

    new course update

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+console.log(printTeacher('John', 'Doe'));

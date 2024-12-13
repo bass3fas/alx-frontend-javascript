3d9e2cec8e0b45919516ec7a57bc26748e7d43af
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-12-13 11:43:05 +0400

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,4 +19,39 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
     return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-console.log(printTeacher('John', 'Doe'));
+interface StudentConstructor {
+    new(firstName: string, lastName: string): Student;
+}
+
+interface Student {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// Using the StudentConstructor interface to type-check the constructor
+const createStudent = (ctor: StudentConstructor, firstName: string, lastName: string): Student => {
+    return new ctor(firstName, lastName);
+};
+
+const student = createStudent(StudentClass, 'Jane', 'Doe');
+console.log(student.displayName()); // Output: Jane
